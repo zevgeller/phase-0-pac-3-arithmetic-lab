708bd64c63fca7ee5d0178decae8a0c70c42c867
Zev <zevgeller@gmail.com>
2022-05-27 04:46:54 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    a + b;
+    return (a + b);
+}
+function subtract(a, b) {
+    a - b;
+    return (a - b);
+}
+function multiply(a, b) {
+    a * b;
+    return (a * b);
+}
+function divide(a, b) {
+    a / b;
+    return (a / b);
+}
+function increment(n) {
+    n++;
+    return (n++);
+}function decrement(n) {
+    n--;
+    return (n--);
+}
+function makeInt(n) {
+    return (parseInt(n,10));
+}
+function preserveDecimal(n) {
+    return (parseFloat(n));
+}
